31b3b3d4555263d83f8fbd68d30b5dbb1a7c869d
임준영 <juny@lguplus.co.kr>
2023-04-25 15:16:41 +0900

    Payload에 PR 수정한 유저 닉네임 추가
    Detail
    ======
     -

--- github.go
+++ github.go
@@ -2,16 +2,17 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"net/http"
 
+	"github.com/lgu-it-sre/mm-github-pr/config"
 	"github.com/lgu-it-sre/mm-github-pr/github"
 	"github.com/lgu-it-sre/mm-github-pr/util"
 )
 
 func (p *Plugin) handlePullRequestEvent(w http.ResponseWriter, r *http.Request) {
 	var pr github.PullRequestEvent
-	err := util.DecodeJSONBody(w, r, &pr)
-	if err != nil {
+	if err := util.DecodeJSONBody(w, r, &pr); err != nil {
 		var mr *util.MalformedRequest
 		if errors.As(err, &mr) {
 			http.Error(w, mr.Msg, mr.Status)
@@ -21,9 +22,31 @@ func (p *Plugin) handlePullRequestEvent(w http.ResponseWriter, r *http.Request) 
 		return
 	}
 
-	payload := "이름" + ` (` + pr.Sender.Login + `) ` + pr.Action + ` merge request ` + `[` + pr.PullRequest.Title + `](` + pr.PullRequest.HtmlURL + `) in [` + pr.PullRequest.Head.Repo.FullName + `](` + pr.PullRequest.Head.Repo.HTML + `)`
-	// for _, username := range pr.GetReviewers() {
-	// 	p.createPost(username, "message", "title", pr.PullRequest.HtmlURL, "description")
-	// }
-	p.createPost("admin", payload, pr.PullRequest.Title, pr.PullRequest.HtmlURL, pr.PullRequest.Body)
+	var sender_nickname string
+	sender_username := pr.Sender.Login
+	pr_action := pr.Action
+	pr_title := pr.PullRequest.Title
+	pr_url := pr.PullRequest.HtmlURL
+	pr_repo_fullname := pr.PullRequest.Head.Repo.FullName
+	pr_repo_html := pr.PullRequest.Head.Repo.HTML
+	pr_body := pr.PullRequest.Body
+	user, err := config.Mattermost.GetUserByUsername(sender_username)
+	if err != nil || user.Nickname == "" {
+		sender_nickname = sender_username
+	} else {
+		sender_nickname = user.Nickname
+	}
+
+	payload := fmt.Sprintf(
+		"%s (%s) %s merge request [%s](%s) in [%s](%s)",
+		sender_nickname,
+		sender_username,
+		pr_action,
+		pr_title,
+		pr_url,
+		pr_repo_fullname,
+		pr_repo_html)
+	for _, username := range pr.GetReviewers() {
+		p.createPost(username, payload, pr_title, pr_url, pr_body)
+	}
 }
