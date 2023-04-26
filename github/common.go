27573953bd2b563c5f9ed9792cf487c24065cb2a
임준영 <juny@lguplus.co.kr>
2023-04-26 14:08:36 +0900

    GitHub Event별로 Response 객체 분리
    Detail
    ======
     -

--- github/common.go
+++ github/common.go
@@ -136,138 +136,116 @@ type repo struct {
 	MergeCommitTitle          string   `json:"merge_commit_title"`
 }
 
+type organization struct {
+	Login         string `json:"login"`
+	ID            int    `json:"id"`
+	Node          string `json:"node_id"`
+	URL           string `json:"url"`
+	Repos         string `json:"repos_url"`
+	Evnts         string `json:"events_url"`
+	Hooks         string `json:"hooks_url"`
+	Issues        string `json:"issues_url"`
+	Members       string `json:"members_url"`
+	PublicMembers string `json:"public_members_url"`
+	Avatar        string `json:"avatar_url"`
+	Desc          string `json:"description"`
+}
+
+type label struct {
+	ID    int    `json:"id"`
+	Node  string `json:"node_id"`
+	URL   string `json:"url"`
+	Name  string `json:"name"`
+	Desc  string `json:"description"`
+	Color string `json:"color"`
+	Def   bool   `json:"default"`
+}
+
 type href struct {
 	Href string `json:"href"`
 }
 
-type PullRequestEvent struct {
-	Action      string `json:"action"`
-	Number      int    `json:"number"`
-	PullRequest struct {
-		URL            string `json:"url"`
-		ID             int    `json:"id"`
-		NodeID         string `json:"node_id"`
-		HtmlURL        string `json:"html_url"`
-		DiffURL        string `json:"diff_url"`
-		PatchURL       string `json:"patch_url"`
-		IssueURL       string `json:"issue_url"`
-		Number         int    `json:"number"`
-		State          string `json:"state"`
-		Locked         bool   `json:"locked"`
-		Title          string `json:"title"`
-		User           user   `json:"user"`
-		Body           string `json:"body"`
-		CreatedAt      string `json:"created_at"`
-		UpdatedAt      string `json:"updated_at"`
-		ClosedAt       string `json:"closed_at"`
-		MergedAt       string `json:"merged_at"`
-		MergeCommitSha string `json:"merge_commit_sha"`
-		Assignee       user   `json:"assignee"`
-		Assignees      []user `json:"assignees"`
-		Reviewers      []user `json:"requested_reviewers"`
-		Team           []team `json:"requested_teams"`
-		Label          []struct {
-			ID    int    `json:"id"`
-			Node  string `json:"node_id"`
-			URL   string `json:"url"`
-			Name  string `json:"name"`
-			Desc  string `json:"description"`
-			Color string `json:"color"`
-			Def   bool   `json:"default"`
-		} `json:"labels"`
-		Milestone struct {
-			URL          string `json:"url"`
-			HTML         string `json:"html_url"`
-			Labels       string `json:"labels_url"`
-			ID           int    `json:"id"`
-			NodeID       string `json:"node_id"`
-			Number       int    `json:"number"`
-			State        string `json:"state"`
-			Title        string `json:"title"`
-			Desc         string `json:"description"`
-			Creator      user   `json:"creator"`
-			OpenIssues   int    `json:"open_issues"`
-			ClosedIssues int    `json:"closed_issues"`
-			Created      string `json:"created_at"`
-			Updated      string `json:"updated_at"`
-			Closed       string `json:"closed_at"`
-			Due          string `json:"due_on"`
-		} `json:"milestone"`
-		Draft       bool   `json:"draft"`
-		CommitsURL  string `json:"commits_url"`
-		ReviewsURL  string `json:"review_comments_url"`
-		ReviewURL   string `json:"review_comment_url"`
-		CommentsURL string `json:"comments_url"`
-		Statuses    string `json:"statuses_url"`
-		Head        struct {
-			Label string `json:"label"`
-			Ref   string `json:"ref"`
-			SHA   string `json:"sha"`
-			User  user   `json:"user"`
-			Repo  repo   `json:"repo"`
-		} `json:"head"`
-		Base struct {
-			Label string `json:"label"`
-			Ref   string `json:"ref"`
-			SHA   string `json:"sha"`
-			User  user   `json:"user"`
-			Repo  repo   `json:"repo"`
-		} `json:"base"`
-		Links struct {
-			Self     href `json:"self"`
-			Html     href `json:"html"`
-			Issue    href `json:"issue"`
-			Comments href `json:"comments"`
-			Review   href `json:"review_comments"`
-			Review2  href `json:"review_comment"`
-			Commits  href `json:"commits"`
-			Status   href `json:"statuses"`
-		} `json:"_links"`
-		AuthorAssociation   string `json:"author_association"`
-		AutoMerge           bool   `json:"auto_merge"`
-		ActiveLockReason    string `json:"active_lock_reason"`
-		Merged              bool   `json:"merged"`
-		Mergable            bool   `json:"mergeable"`
-		Rebaseable          bool   `json:"rebaseable"`
-		MergeableState      string `json:"mergeable_state"`
-		MergedBy            user   `json:"merged_by"`
-		Comments            int    `json:"comments"`
-		ReviewComments      int    `json:"review_comments"`
-		MaintainerCanModify bool   `json:"maintainer_can_modify"`
-		Commits             int    `json:"commits"`
-		Additions           int    `json:"additions"`
-		Deletions           int    `json:"deletions"`
-		ChangedFiles        int    `json:"changed_files"`
-	} `json:"pull_request"`
-	Changes struct {
-		Body struct {
-			From string `json:"from"`
-		} `json:"body"`
-	} `json:"changes"`
-	Before       string `json:"before"`
-	After        string `json:"after"`
-	Repository   repo   `json:"repository"`
-	Organization struct {
-		Login         string `json:"login"`
-		ID            int    `json:"id"`
-		Node          string `json:"node_id"`
-		URL           string `json:"url"`
-		Repos         string `json:"repos_url"`
-		Evnts         string `json:"events_url"`
-		Hooks         string `json:"hooks_url"`
-		Issues        string `json:"issues_url"`
-		Members       string `json:"members_url"`
-		PublicMembers string `json:"public_members_url"`
-		Avatar        string `json:"avatar_url"`
-		Desc          string `json:"description"`
-	} `json:"organization"`
-	Sender user `json:"sender"`
+type milestone struct {
+	URL          string `json:"url"`
+	HTML         string `json:"html_url"`
+	Labels       string `json:"labels_url"`
+	ID           int    `json:"id"`
+	NodeID       string `json:"node_id"`
+	Number       int    `json:"number"`
+	State        string `json:"state"`
+	Title        string `json:"title"`
+	Desc         string `json:"description"`
+	Creator      user   `json:"creator"`
+	OpenIssues   int    `json:"open_issues"`
+	ClosedIssues int    `json:"closed_issues"`
+	Created      string `json:"created_at"`
+	Updated      string `json:"updated_at"`
+	Closed       string `json:"closed_at"`
+	Due          string `json:"due_on"`
+}
+type reactions struct {
+	URL      string `json:"url"`
+	Total    int    `json:"total_count"`
+	PlusOne  int    `json:"+1"`
+	MinusOne int    `json:"-1"`
+	Laugh    int    `json:"laugh"`
+	Hooray   int    `json:"hooray"`
+	Confused int    `json:"confused"`
+	Heart    int    `json:"heart"`
+	Rocket   int    `json:"rocket"`
+	Eyes     int    `json:"eyes"`
 }
 
-func (p *PullRequestEvent) GetReviewers() []string {
-	var reviewers []string
-	for _, reviewer := range p.PullRequest.Reviewers {
-		reviewers = append(reviewers, reviewer.Login)
-	}
-	return reviewers
+type issue struct {
+	URL         string    `json:"url"`
+	RepoURL     string    `json:"repository_url"`
+	LabelsURL   string    `json:"labels_url"`
+	CommentsURL string    `json:"comments_url"`
+	EventsURL   string    `json:"events_url"`
+	HtmlURL     string    `json:"html_url"`
+	ID          int       `json:"id"`
+	NodeID      string    `json:"node_id"`
+	Number      int       `json:"number"`
+	Title       string    `json:"title"`
+	User        user      `json:"user"`
+	Labels      []label   `json:"labels"`
+	State       string    `json:"state"`
+	Locked      bool      `json:"locked"`
+	Assignee    user      `json:"assignee"`
+	Assignees   []user    `json:"assignees"`
+	Milestone   milestone `json:"milestone"`
+	Comments    int       `json:"comments"`
+	Created     string    `json:"created_at"`
+	Updated     string    `json:"updated_at"`
+	Closed      string    `json:"closed_at"`
+	AuthorAssoc string    `json:"author_association"`
+	ActiveLock  string    `json:"active_lock_reason"`
+	Draft       bool      `json:"draft"`
+	PullRequest struct {
+		URL      string `json:"url"`
+		HtmlURL  string `json:"html_url"`
+		DiffURL  string `json:"diff_url"`
+		PatchURL string `json:"patch_url"`
+		MergedAt string `json:"merged_at"`
+	} `json:"pull_request"`
+	Body        string    `json:"body"`
+	Reactions   reactions `json:"reactions"`
+	TimelineURL string    `json:"timeline_url"`
+	Performed   string    `json:"performed_via_github_app"`
+	StateReason string    `json:"state_reason"`
+}
+
+type comment struct {
+	URL         string    `json:"url"`
+	HTMLURL     string    `json:"html_url"`
+	IssueURL    string    `json:"issue_url"`
+	ID          int       `json:"id"`
+	NodeID      string    `json:"node_id"`
+	User        user      `json:"user"`
+	Created     string    `json:"created_at"`
+	Updated     string    `json:"updated_at"`
+	AuthorAssoc string    `json:"author_association"`
+	Body        string    `json:"body"`
+	Reactions   reactions `json:"reactions"`
+	Performed   string    `json:"performed_via_github_app"`
 }
---
+++ github/pullrequest.go
@@ -0,0 +1,103 @@
+package github
+
+type PullRequestEvent struct {
+	Action      string `json:"action"`
+	Number      int    `json:"number"`
+	PullRequest struct {
+		URL            string `json:"url"`
+		ID             int    `json:"id"`
+		NodeID         string `json:"node_id"`
+		HtmlURL        string `json:"html_url"`
+		DiffURL        string `json:"diff_url"`
+		PatchURL       string `json:"patch_url"`
+		IssueURL       string `json:"issue_url"`
+		Number         int    `json:"number"`
+		State          string `json:"state"`
+		Locked         bool   `json:"locked"`
+		Title          string `json:"title"`
+		User           user   `json:"user"`
+		Body           string `json:"body"`
+		CreatedAt      string `json:"created_at"`
+		UpdatedAt      string `json:"updated_at"`
+		ClosedAt       string `json:"closed_at"`
+		MergedAt       string `json:"merged_at"`
+		MergeCommitSha string `json:"merge_commit_sha"`
+		Assignee       user   `json:"assignee"`
+		Assignees      []user `json:"assignees"`
+		Reviewers      []user `json:"requested_reviewers"`
+		Team           []team `json:"requested_teams"`
+		Label          []struct {
+			ID    int    `json:"id"`
+			Node  string `json:"node_id"`
+			URL   string `json:"url"`
+			Name  string `json:"name"`
+			Desc  string `json:"description"`
+			Color string `json:"color"`
+			Def   bool   `json:"default"`
+		} `json:"labels"`
+		Milestone   milestone `json:"milestone"`
+		Draft       bool      `json:"draft"`
+		CommitsURL  string    `json:"commits_url"`
+		ReviewsURL  string    `json:"review_comments_url"`
+		ReviewURL   string    `json:"review_comment_url"`
+		CommentsURL string    `json:"comments_url"`
+		Statuses    string    `json:"statuses_url"`
+		Head        struct {
+			Label string `json:"label"`
+			Ref   string `json:"ref"`
+			SHA   string `json:"sha"`
+			User  user   `json:"user"`
+			Repo  repo   `json:"repo"`
+		} `json:"head"`
+		Base struct {
+			Label string `json:"label"`
+			Ref   string `json:"ref"`
+			SHA   string `json:"sha"`
+			User  user   `json:"user"`
+			Repo  repo   `json:"repo"`
+		} `json:"base"`
+		Links struct {
+			Self     href `json:"self"`
+			Html     href `json:"html"`
+			Issue    href `json:"issue"`
+			Comments href `json:"comments"`
+			Review   href `json:"review_comments"`
+			Review2  href `json:"review_comment"`
+			Commits  href `json:"commits"`
+			Status   href `json:"statuses"`
+		} `json:"_links"`
+		AuthorAssociation   string `json:"author_association"`
+		AutoMerge           bool   `json:"auto_merge"`
+		ActiveLockReason    string `json:"active_lock_reason"`
+		Merged              bool   `json:"merged"`
+		Mergable            bool   `json:"mergeable"`
+		Rebaseable          bool   `json:"rebaseable"`
+		MergeableState      string `json:"mergeable_state"`
+		MergedBy            user   `json:"merged_by"`
+		Comments            int    `json:"comments"`
+		ReviewComments      int    `json:"review_comments"`
+		MaintainerCanModify bool   `json:"maintainer_can_modify"`
+		Commits             int    `json:"commits"`
+		Additions           int    `json:"additions"`
+		Deletions           int    `json:"deletions"`
+		ChangedFiles        int    `json:"changed_files"`
+	} `json:"pull_request"`
+	Changes struct {
+		Body struct {
+			From string `json:"from"`
+		} `json:"body"`
+	} `json:"changes"`
+	Before     string       `json:"before"`
+	After      string       `json:"after"`
+	Repository repo         `json:"repository"`
+	Org        organization `json:"organization"`
+	Sender     user         `json:"sender"`
+}
+
+func (p *PullRequestEvent) GetReviewers() []string {
+	var reviewers []string
+	for _, reviewer := range p.PullRequest.Reviewers {
+		reviewers = append(reviewers, reviewer.Login)
+	}
+	return reviewers
+}
