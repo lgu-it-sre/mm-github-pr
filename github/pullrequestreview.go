14e5e3641610b1e86568e69982cd8c2ce8042f8c
임준영 <juny@lguplus.co.kr>
2023-04-27 18:45:15 +0900

    PR Review `.changes` 추가
    Detail
    ======
     -

--- github/pullrequestreview.go
+++ github/pullrequestreview.go
@@ -1,13 +1,21 @@
 package github
 
 type PullRequestReviewEvent struct {
-	Action      string       `json:"action"`
-	Review      review       `json:"review"`
-	PullRequest pullRequest  `json:"pull_request"`
-	Repository  repo         `json:"repository"`
-	Org         organization `json:"organization"`
-	Ent         enterprise   `json:"enterprise"`
-	Sender      user         `json:"sender"`
+	Action      string      `json:"action"`
+	Review      review      `json:"review"`
+	PullRequest pullRequest `json:"pull_request"`
+	Changes     struct {
+		Title struct {
+			From string `json:"from"`
+		} `json:"title"`
+		Body struct {
+			From string `json:"from"`
+		} `json:"body"`
+	} `json:"changes"`
+	Repository repo         `json:"repository"`
+	Org        organization `json:"organization"`
+	Ent        enterprise   `json:"enterprise"`
+	Sender     user         `json:"sender"`
 }
 
 func (p *PullRequestReviewEvent) GetReviewers() []string {
